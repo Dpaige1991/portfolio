1a6367cadd5b5a900643de41e44ac08a78dcc878
Dominique D Paige <dominique.paige@outlook.com>
2022-08-30 19:51:50 -0500

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+
